59c7eb1d5c1d5533fd412d4570dfd43805c4b673
Ray <mindcultivateinfo@gmail.com>
2026-03-17 17:37:03 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  // base: process.env.VITE_BASE_PATH || "/ray-portifolio"
+  base: process.env.VITE_BASE_PATH || "/ray-portifolio"
 })
